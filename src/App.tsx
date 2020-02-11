b2d458cf2caafff0f5c8f7bc42209f55ab399850
matteokjh <429797371@qq.com>
2020-02-11 16:27:19 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MonacoEditor from "react-monaco-editor";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [code, setCode] = useState('')
+
+	// methods
+	const codeChange = (code: string) => {
+		console.log(code)
+	}
+
+    return (
+        <div className="App">
+            <MonacoEditor
+                width="600"
+                height="800"
+                language="javascript"
+                theme="vs-dark"
+                value={code}
+				onChange={codeChange}
+				options={{
+					selectOnLineNumbers: true
+				}}
+            />
+        </div>
+    );
+};
 
 export default App;
